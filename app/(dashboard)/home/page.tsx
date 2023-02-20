12a73aa3949bf79d2da42d25410ab61a70f9acfb
joycehwchan <joycehwchan@gmail.com>
2023-02-20 12:59:21 +0900

    setup project page

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -11,7 +11,7 @@ import ProjectCard from "@/components/ProjectCard";
 import TaskCard from "@/components/TaskCard";
 
 const getData = async () => {
-  await delay(2000);
+  await delay(1000);
   const user = await getUserFromCookie(cookies());
   const projects = await db.project.findMany({
     where: {
